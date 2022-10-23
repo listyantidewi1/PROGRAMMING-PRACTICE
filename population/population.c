06d9097ef7a1e671f33e4cacf2b8913e45edc04a
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-23 10:48:25 +0000

    Sun, Oct 23, 2022, 10:48 AM +00:00

--- population/population.c
+++ population/population.c
@@ -12,21 +12,14 @@ int main(void)
 
     while (start_size < 9)
     {
-        //printf("Start size: ");
-        //scanf("%i", &start_size);
         start_size = get_int("Start size: ");
     }
 
     // TODO: Prompt for end size
     while (end_size < start_size)
     {
-        //printf("End size : ");
-        //scanf("%i", &end_size);
         end_size = get_int("End size: ");
     }
-
-    //printf("\nStart size %i", start_size);
-    //printf("\nEnd size %i", end_size);
 
     curr_size = start_size;
 
