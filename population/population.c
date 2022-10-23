6b1b6671775136aae719d964ad74f6c282ed6290
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-23 09:28:35 +0000

    Sun, Oct 23, 2022, 9:28 AM +00:00

--- population/population.c
+++ population/population.c
@@ -19,7 +19,7 @@ int main(void)
     }
 
     // TODO: Prompt for end size
-    while (end_size <= 9)
+    while (end_size <= start_size)
     {
         printf("End size : ");
         scanf("%d", &end_size);
@@ -32,7 +32,7 @@ int main(void)
 
     // TODO: Calculate number of years until we reach threshold
 
-    while (curr_size < end_size)
+    while (curr_size <= end_size)
     {
         //born = curr_size / 3;
         //death = curr_size / 4;
