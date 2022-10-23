a4b0d06698b86d7e87c7c7044a49849b7c310059
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-23 07:20:58 +0000

    Sun, Oct 23, 2022, 7:20 AM +00:00

--- population/population.c
+++ population/population.c
@@ -4,6 +4,12 @@
 int main(void)
 {
     // TODO: Prompt for start size
+    int population_size;
+    printf("Enter population size: ");
+    scanf("%d",&population_size);
+    while(population_size >= 9){
+        printf("ok");
+    };
 
     // TODO: Prompt for end size
 
