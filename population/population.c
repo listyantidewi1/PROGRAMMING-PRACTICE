2401f15f694e28e21b44e3e50ea6c8f29df949f9
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-23 09:17:59 +0000

    Sun, Oct 23, 2022, 9:17 AM +00:00

--- population/population.c
+++ population/population.c
@@ -5,25 +5,42 @@ int main(void)
 {
     // TODO: Prompt for start size
     int start_size = 0;
-    int end_size =0;
+    int end_size = 0;
+    int curr_size;
+    int year = 0;
+    int born = 0;
+    int death = 0;
 
-    while(population_size < 9){
+    while(start_size < 9){
         printf("Start size: ");
         scanf("%d", &start_size);
-        while(end_size < = 9){
-            printf("End size : ");
-            scanf("%d", &end_size);
-        }
-
 
     }
 
-
-
-
     // TODO: Prompt for end size
+    while(end_size <= 9){
+        printf("End size : ");
+        scanf("%d", &end_size);
+    }
+
+    printf("\nStart size %d", start_size);
+    printf("\nEnd size %d\n", end_size);
+
+    curr_size = start_size;
 
     // TODO: Calculate number of years until we reach threshold
 
+    while(curr_size < end_size){
+
+        born = curr_size/3;
+        death = curr_size/4;
+        curr_size = curr_size + born - death;
+        year++;
+        printf("\nCurrent size: %d", curr_size);
+        printf("\nborn: %d", born);
+        printf("\ndeath: %d", death);
+    }
+
     // TODO: Print number of years
+    printf("\nYear\n : %d",year);
 }
