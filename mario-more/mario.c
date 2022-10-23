558cc9d60513136ae80210fe318bfebb46be0db0
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-23 13:12:17 +0000

    Sun, Oct 23, 2022, 1:12 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,22 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int height = 0;
+    int i,j,k,l;
+
+    while (height <= 0)
+    {
+        height = get_int("Height: ");
+    }
+
+    for(i=0; i<height; i++)
+    {
+        printf("\n");
+        for(j=0; j<i; j++)
+        {
+            printf("#");
+        }
+    }
 }
