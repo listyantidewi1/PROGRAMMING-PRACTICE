dde1f3bfc3a29a1a765bff87cfe4c8e0f00ad22d
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-23 10:36:38 +0000

    Sun, Oct 23, 2022, 10:36 AM +00:00

--- population/population.c
+++ population/population.c
@@ -5,26 +5,28 @@
 int main(void)
 {
     // TODO: Prompt for start size
-    int start_size = 0;
-    int end_size = 0;
+    //int start_size = 0;
+    //int end_size = 0;
     int curr_size; //current population size
     int year = 0; //number of years to reach the end size
 
     while (start_size < 9)
     {
-        printf("Start size: ");
-        scanf("%i", &start_size);
+        //printf("Start size: ");
+        //scanf("%i", &start_size);
+        int start_size = get_int("Start size : ");
     }
 
     // TODO: Prompt for end size
     while (end_size < start_size)
     {
-        printf("End size : ");
-        scanf("%i", &end_size);
+        //printf("End size : ");
+        //scanf("%i", &end_size);
+        int end_size = get_int("End size : ");
     }
 
-    printf("\nStart size %i", start_size);
-    printf("\nEnd size %i", end_size);
+    //printf("\nStart size %i", start_size);
+    //printf("\nEnd size %i", end_size);
 
     curr_size = start_size;
 
