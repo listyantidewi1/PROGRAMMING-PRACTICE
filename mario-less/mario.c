2caafc746424b334c91863130105d8ee6b036ade
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-24 04:30:54 +0000

    Mon, Oct 24, 2022, 4:30 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,29 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int height = 0;
+    int i,j,k,l;
+
+    while ((height <= 0) || (height > 8))
+    {
+        height = get_int("Height: ");
+    }
+
+    for(i=0; i<height; i++)
+    {
+        printf("\n");
+
+        for(k=height; k>=0; k--)
+        {
+            printf(" ");
+        }
+
+        for(j=0; j<=i; j++)
+        {
+            printf("#");
+        }
+    }
+    printf("\n");
 }
