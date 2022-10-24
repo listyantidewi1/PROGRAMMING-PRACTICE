d9fa3035c0e9fe39737a906a48dfc6c7caca57ff
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-24 07:57:41 +0000

    Mon, Oct 24, 2022, 7:57 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,39 +3,30 @@
 
 int main(void)
 {
-    long n, first, sequence=100;
+    long n, first, second, sequence=100, sequence2=10;
     //prompt input
     long number = get_long ("Number: ");
 
     //checksum
 
-    //get two last digit
-    long twoLast = number % 100;
-
-    //get first digit
-
 
     //get first digit of every other number
-    //sequence: 10, 1000, ..
+    //sequence: 100, 10000, ..
     //sequence * 100
-
     for(n = 0; n < 17; n++)
     {
         twoLast = number % sequence;
         sequence = sequence * 100;
-        printf("twolast = %li\n", twoLast);
+        //printf("twolast = %li\n", twoLast);
 
         first = twoLast;
         while (first >= 10)
         {
             first = first / 10;
         }
-        printf("first = %li\n", first);
+        //printf("first = %li\n", first);
     }
 
-    // printf("Number: %li\n", number);
-    // printf("Twolast: %li\n", twoLast);
-    // printf("First: %li\n", first);
 
 
 }
