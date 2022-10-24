7125d0a64436ec3a5c3ce5a47265e4e34e968e8b
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-24 07:58:59 +0000

    Mon, Oct 24, 2022, 7:58 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,14 +3,13 @@
 
 int main(void)
 {
-    long n, first, second, sequence=100, sequence2=10;
+    long n, underlined, not_underlined, sequence=100, sequence2=10;
     //prompt input
     long number = get_long ("Number: ");
 
     //checksum
 
-
-    //get first digit of every other number
+    //get underlined digit of every other number
     //sequence: 100, 10000, ..
     //sequence * 100
     for(n = 0; n < 17; n++)
@@ -19,12 +18,12 @@ int main(void)
         sequence = sequence * 100;
         //printf("twolast = %li\n", twoLast);
 
-        first = twoLast;
-        while (first >= 10)
+        underlined = twoLast;
+        while (underlined >= 10)
         {
-            first = first / 10;
+            underlined = underlined / 10;
         }
-        //printf("first = %li\n", first);
+        //printf("underlined = %li\n", underlined);
     }
 
 
