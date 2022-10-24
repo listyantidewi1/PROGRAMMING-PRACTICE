65f9091e4ee394d9c338b19086f5bb4d856711b6
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-24 08:01:47 +0000

    Mon, Oct 24, 2022, 8:01 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    long n, underlined, not_underlined, sequence=100, sequence2=10;
+    long n, n_last, underlined, not_underlined, sequence=100, sequence2=10;
     //prompt input
     long number = get_long ("Number: ");
 
@@ -14,11 +14,11 @@ int main(void)
     //sequence * 100
     for(n = 0; n < 17; n++)
     {
-        twoLast = number % sequence;
+        n_last = number % sequence;
         sequence = sequence * 100;
-        //printf("twolast = %li\n", twoLast);
+        //printf("n_last = %li\n", n_last);
 
-        underlined = twoLast;
+        underlined = n_last;
         while (underlined >= 10)
         {
             underlined = underlined / 10;
