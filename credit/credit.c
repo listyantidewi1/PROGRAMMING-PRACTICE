3e921b083dc90af458a7b5a446e158d8c25d3886
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-24 17:12:21 +0000

    Mon, Oct 24, 2022, 5:12 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,58 +1,111 @@
-#include <cs50.h>
+/*
+
+Most cards use an algorithm invented by Hans Peter Luhn of IBM. According to Luhn’s algorithm, you can determine if a credit card number is (syntactically) valid as follows:
+
+1. Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products’ digits together.
+2. Add the sum to the sum of the digits that weren’t multiplied by 2.
+3. If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!
+
+*/
 #include <stdio.h>
-#include <math.h>
+#include <cs50.h>
 
-int main(void)
-{
-    long n, n_last, n_last2, underlined, not_underlined, sequence=100, sequence2=10;
+bool checkSum(long int, int);
+int checkLength(long int);
+string checkCardType(long int);
 
-    //prompt input
-    long number = get_long ("Number: ");
+int main(void) {
+    long int creditCard;
 
-    //count number of digit
-    int count = (number == 0) ? 1  : (log10(number) + 1);
-    int arr_underlined[count/2];
-    //printf("num digit: %i", count);
+    do{
+        creditCard = get_long("Number: ");
+    }while(creditCard<1);
 
-    //checksum
+    int length = checkLength(creditCard);
+    if (length == 0) {
+        printf("INVALID\n");
+        return 0;
+    }
 
-    //get underlined digit / every other number start from the second to last digit
-    //sequence: 100, 10000, ..
-    //sequence * 100
-    for(n = 0; n < count/2; n++)
-    {
-        n_last = number % sequence;
-        sequence = sequence * 100;
-        //printf("n_last = %li\n", n_last);
+    int checkSumValue = checkSum(creditCard, length);
+    if (checkSumValue != 1) {
+        printf("INVALID\n");
+        return 0;
+    }
 
-        underlined = n_last;
-        while (underlined >= 10)
-        {
-            underlined = underlined / 10;
+    string cardType = checkCardType(creditCard);
+    printf("%s\n", cardType);
 
+    return 0;
+}
+
+bool checkSum(long int creditCard, int length) {
+    int i=0;
+    int temp=0;
+    int first=0;
+    int second=0;
+    int final=0;
+
+    while(creditCard){
+        if (i%2 != 0) {
+            first = ((creditCard%10)*2);
+            if (first>9){
+                while(first){
+                    temp += first%10;
+                    first /= 10;
+                }
+            }
+            temp += first;
+        } else {
+            second += creditCard%10;
         }
-        //printf("underlined = %li\n", underlined);
-        arr_underlined[n] = underlined;
+        creditCard /= 10;
+        i++;
     }
 
-    //print array of underlined numbers
-    for(n=0; n < (sizeof arr_underlined / sizeof arr_underlined[0]); n++)
-    {
-        printf("%i", arr_underlined[n]);
+    temp += second;
+
+    final = temp%10;
+
+    if (final == 0){
+        return 1;
     }
 
-    //multiply each array of underlined number by two, and calculate the sum
-    int sum1 = 0;
-    int sum2 =0;
+    return 0;
+}
 
-    for (n=0; n < (sizeof arr_underlined / sizeof arr_underlined[0]); n++)
-    {
-        sum1 = arr_underlined[n]*2;
-        sum2 = sum2 + sum1;
+int checkLength(long int creditCard) {
+  int i=1;
+  while(creditCard>9){
+      i++;
+      creditCard/=10;
+  }
+  if (i<13 || i>16) {
+      return 0;
+  }
+  return i;
+}
+
+string checkCardType(long int creditCard) {
+    // number2 will hold the first two digits.
+    long int number1 = creditCard, number2 = creditCard;
+    while (creditCard){
+        number2 = number1;
+        number1 = creditCard;
+        creditCard /= 10;
     }
 
-    //print sum1
-    printf("\nsum2 = %i\n", sum2);
+    if (number2 == 34 || number2 ==37) {
+        return "AMEX";
+    }
 
+    if (number2 >= 51 && number2 <= 55) {
+        return "MASTERCARD";
+    }
 
+    if (number2 >= 40 && number2 <=49) {
+        return "VISA";
+    }
+
+    return "INVALID";
 }
