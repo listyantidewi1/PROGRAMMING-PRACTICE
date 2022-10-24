f3da15d448905b6d0b6b73dd40aae28426d0fac6
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-24 04:51:59 +0000

    Mon, Oct 24, 2022, 4:51 AM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -4,27 +4,27 @@
 int main(void)
 {
     int height = 0;
-    int i, j, k, l;
+    int i, j, k; //initialize variables
 
+    //ask for height
     while ((height <= 0) || (height > 8))
     {
         height = get_int("Height: ");
     }
 
+    //loop for the numbers of rows
     for (i = 0; i < height; i++)
     {
-
-
-        for (k = height; k > i + 1; k--)
+        for (k = height; k > i + 1; k--) //loops for the numbers of spaces
         {
             printf(" ");
         }
 
-        for (j = 0; j <= i; j++)
+        for (j = 0; j <= i; j++) //loops for the numbers of #
         {
             printf("#");
         }
-        printf("\n");
+        printf("\n"); //new row
     }
 
 }
