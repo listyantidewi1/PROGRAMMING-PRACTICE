fea95d0b5d13273dbc2dad36fae939e10a60ad5d
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-24 04:48:26 +0000

    Mon, Oct 24, 2022, 4:48 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -4,18 +4,20 @@
 int main(void)
 {
     int height = 0;
-    int i, j, k, l;
+    int i, j, k, l; //initialize variables
 
+
+    //Prompt height 1-8
     while ((height <= 0) || (height > 8))
     {
         height = get_int("Height: ");
     }
 
+    //loop for the numbers of rows
     for (i = 0; i < height; i++)
     {
-
-
-        for (k = height; k > i+1; k--)
+        //loop for the numbers of space
+        for (k = height; k > i + 1; k--)
         {
             printf(" ");
         }
