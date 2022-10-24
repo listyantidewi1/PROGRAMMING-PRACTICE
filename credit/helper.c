c4850aadf7f4fe9694359fd3aad318de9a85906e
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-24 17:00:41 +0000

    Mon, Oct 24, 2022, 5:00 PM +00:00

--- credit/helper.c
+++ credit/helper.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <math.h>
+#include <cs50.h>
 
 int main(void)
 {
     long number = get_long("Enter number: ");
     int count = (number == 0) ? 1  : (log10(number) + 1);
 
-    for(n = 0; n < count/2; n++)
-    {
-        int i = (number % 10)*2;
+    int i=0;
+
+    while((i%2) != 0){
+        int lastDigit = number % 10;
+        printf("%i",lastDigit);
+        i++;
     }
 
 }
