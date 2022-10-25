e3ee2bf2adc2704c9f1298e31998db54a4ac5196
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-25 00:18:39 +0000

    Tue, Oct 25, 2022, 12:18 AM +00:00

--- credit/credit2.c
+++ credit/credit2.c
@@ -7,6 +7,7 @@ int main(void)
 
     //prompt input
     long number = get_long ("Number: ");
+    long n = number;
 
     //count number of digit
     int length = (number == 0) ? 1  : (log10(number) + 1);
@@ -32,6 +33,15 @@ int main(void)
     while (n > 10) //get leftmost digit
     {
         n = n /10;
+    }
+
+    printf("Sum: %li\nLeftmost: %li\n", sum, n);
+
+    if (sum % 10)
+    {
+        printf("Invalid");
+    }else{
+        printf("Valid");
     }
 
 
