77684262d3fc232c335dd66765b2bfb91db1c16e
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-25 00:40:03 +0000

    Tue, Oct 25, 2022, 12:40 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,116 +1,76 @@
-/*
-
-Most cards use an algorithm invented by Hans Peter Luhn of IBM. According to Luhn’s algorithm, you can determine if a credit card number is (syntactically) valid as follows:
-
-1. Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products’ digits together.
-2. Add the sum to the sum of the digits that weren’t multiplied by 2.
-3. If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!
-
-*/
-#include <stdio.h>
 #include <cs50.h>
-
-bool checkSum(long int, int);
-int checkLength(long int);
-string checkCardType(long int);
+#include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
-    long int creditCard;
 
-    do
+    //prompt input
+    long number = get_long("Number: ");
+    long n = number;
+    long n_checktype = number;
+
+    //count number of digit
+    int length = (number == 0) ? 1  : (log10(number) + 1);
+
+    //Luhn Algorithm
+    int sum = 0;
+    sum = number % 10; //add checksum - get last digit
+    number = number / 10; //chop last digit off
+
+    while (number)
     {
-        creditCard = get_long("Number: ");
-    }
-    while(creditCard < 1);
-
-    int length = checkLength(creditCard);
-    if (length == 0)
-    {
-        printf("INVALID\n");
-        return 0;
-    }
-
-    int checkSumValue = checkSum(creditCard, length);
-    if (checkSumValue != 1)
-    {
-        printf("INVALID\n");
-        return 0;
-    }
-
-    string cardType = checkCardType(creditCard);
-    printf("%s\n", cardType);
-
-    return 0;
-}
-
-bool checkSum(long int creditCard, int length) {
-    int i=0;
-    int temp=0;
-    int first=0;
-    int second=0;
-    int final=0;
-
-    while(creditCard){
-        if (i%2 != 0) {
-            first = ((creditCard%10)*2);
-            if (first>9){
-                while(first){
-                    temp += first%10;
-                    first /= 10;
-                }
-            }
-            temp += first;
-        } else {
-            second += creditCard%10;
+        int temp = (number % 10) * 2; //get righmost digit and double it
+        if (temp > 9)
+        {
+            temp = temp - 9;
         }
-        creditCard /= 10;
-        i++;
+        sum = sum + temp; //add digit to sum
+        number = number / 10; //chop rightmost digit off
+        temp = number % 10; //get another rightmost digit
+        sum = sum + temp; //add digit to sum
+        number = number / 10; //chop right digit off
+    }
+    while (n > 10) //get leftmost digit
+    {
+        n = n / 10;
     }
 
-    temp += second;
+    //printf("Sum: %i\nLeftmost: %li\n", sum, n);
 
-    final = temp%10;
+    if (sum % 10)
+    {
+        printf("INVALID\n");
+    }
+    else
+    {
+        //printf("Valid\n");
+        long int number1 = n_checktype, number2 = n_checktype;
+        while (n_checktype)
+        {
+            number2 = number1;
+            number1 = n_checktype;
+            n_checktype /= 10;
+        }
 
-    if (final == 0){
-        return 1;
+        if (number2 == 34 || number2 == 37)
+        {
+            printf("AMEX");
+        }
+
+        if ((number2 >= 51 && number2 <= 55) || number2 == 22)
+        {
+            printf("MASTERCARD");
+        }
+
+        if (number2 >= 40 && number2 <= 49)
+        {
+            printf("VISA");
+        }
+        else
+        {
+            //printf("INVALID");
+        }
     }
 
-    return 0;
-}
-
-int checkLength(long int creditCard) {
-  int i=1;
-  while(creditCard>9){
-      i++;
-      creditCard/=10;
-  }
-  if (i<13 || i>16) {
-      return 0;
-  }
-  return i;
-}
-
-string checkCardType(long int creditCard) {
-    // number2 will hold the first two digits.
-    long int number1 = creditCard, number2 = creditCard;
-    while (creditCard){
-        number2 = number1;
-        number1 = creditCard;
-        creditCard /= 10;
-    }
-
-    if (number2 == 34 || number2 ==37) {
-        return "AMEX";
-    }
-
-    if (number2 >= 51 && number2 <= 55) {
-        return "MASTERCARD";
-    }
-
-    if (number2 >= 40 && number2 <=49) {
-        return "VISA";
-    }
-
-    return "INVALID";
 }
