e22281f6cfd785767a36ecdd90df45e0172ad0bb
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-26 13:38:59 +0000

    Wed, Oct 26, 2022, 1:38 PM +00:00

--- sum/sum.c
+++ sum/sum.c
@@ -4,7 +4,7 @@
 int main(void)
 {
     //storage for 10 integers
-    int number, sum=0; //number is for storing numbers, sum is for storing sum
+    int number, sum=0; //number is for storing numbers we get from the user, sum is for storing sum
     int i = 0; //counter
 
     //prompt user to input data and calculate the sum
