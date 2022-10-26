207c782db52f6388f5dab2b0a754c09feb99ebee
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-26 13:08:23 +0000

    Wed, Oct 26, 2022, 1:08 PM +00:00

---
+++ sum/meow.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    for (int i=0; i<10; i++)
+    {
+        printf("Meow in for loop\n");
+    }
+
+    int j = 0;
+    while(j < 10)
+    {
+        printf("Meow in while loop\n");
+        j++;
+    }
+
+    int k=0;
+    do
+    {
+        printf("Meow in do-while loop\n");
+        k++;
+    }while(k < 10);
+}
