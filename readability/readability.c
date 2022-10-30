18ce435107d13c5e740a480a1402ff451d68cba8
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-30 13:08:41 +0000

    Sun, Oct 30, 2022, 1:08 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -10,15 +10,32 @@ int main(void)
     int num_letters = 0;
     int i = 0;
 
-    while(text[i] != '\0')
+    while (text[i] != '\0')
     {
-        if( ((text[i] >= 65) && (text[i] <= 90)) ||  ((text[i] >= 97) && (text[i] <= 122)))
+        if( ((text[i] >= 65) && (text[i] <= 90)) ||  ((text[i] >= 97) && (text[i] <= 122))) //whenever hits alphabet, increase the word counts
         {
             num_letters++;
         }
         i++;
     }
 
-    printf("Number of letters: %d", num_letters);
+    printf("Number of letters: %d\n", num_letters);
+
+
+    //count the number of words;
+
+    int num_words = 1;
+    i = 0;
+
+    while (text[i] != '\0')
+    {
+        if(text[i] == 32) //whenever hits a space, increase the word count
+        {
+            num_words++;
+        }
+        i++;
+    }
+
+    printf("Number of words: %d\n", num_words);
 
 }
