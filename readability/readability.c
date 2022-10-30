c32e45ea75ee9432205dc6754cec704f7bb641a5
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-30 13:05:04 +0000

    Sun, Oct 30, 2022, 1:05 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,24 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
+    //getting user input
+    string text = get_string("Text: ");
+
+    //count the number of letters
+    int num_letters = 0;
+    int i = 0;
+
+    while(text[i] != '\0')
+    {
+        if( ((text[i] >= 65) && (text[i] <= 90)) ||  ((text[i] >= 97) && (text[i] <= 122)))
+        {
+            num_letters++;
+        }
+        i++;
+    }
+
+    printf("Number of letters: %d", num_letters);
 
 }
