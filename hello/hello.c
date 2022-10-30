3948abe58e313d1ed1a45c8f6e6c3db29be0d095
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-30 08:52:30 +0000

    Sun, Oct 30, 2022, 8:52 AM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -1,9 +1,14 @@
-#include <cs50.h>
-#include <stdio.h>
-//#include <stdin.h>
+  #include <cs50.h>
+  #include <stdio.h>
 
-int main(void)
-{
-    string name = get_string("What's your name? \n");
-    printf("Hello, %s\n", name);
-}
+  int main(int argc, string argv[])
+  {
+      if (argc == 2)
+      {
+          printf("hello, %s\n", argv[1]);
+      }
+      else
+      {
+          printf("hello, world\n");
+      }
+  }  
