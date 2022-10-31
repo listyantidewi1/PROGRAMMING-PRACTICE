ffe05e951d1d9c67e8ba0a4a92d18c42edba3b61
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-31 15:36:52 +0000

    Mon, Oct 31, 2022, 3:36 PM +00:00

--- problem-sets/caesar/caesar.c
+++ problem-sets/caesar/caesar.c
@@ -7,6 +7,14 @@ int main(int argc, string argv[])
 {
     //formula: c = (p+k)%26
     //c = chipertext, k = key, p = plaintext
-    
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+    }
+    else
+    {
+        //check if the arguments are not number
+        
+    }
 
 }
