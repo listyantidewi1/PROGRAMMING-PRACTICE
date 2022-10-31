be027584d6a94fab7bfaf8caa1b9bc5b30e92492
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-31 08:57:40 +0000

    Mon, Oct 31, 2022, 8:57 AM +00:00

--- problem-sets/caesar/caesar.c
+++ problem-sets/caesar/caesar.c
@@ -5,8 +5,13 @@ int main(int argc, string argv[])
 {
     //formula: c = (p+k)%26
     //c = chipertext, k = key, p = plaintext
-    while((argc == 2) && (argv))
+    while(argc==2)
     {
+        string _argv = argv[2];
+        for (int i = 0; length = strlen(_argv); i++)
+        {
+            if (_argv)
+        }
 
     }
 
