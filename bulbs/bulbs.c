091ee0134324e1fc8c56f22d65a28f803f57701e
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-31 00:40:12 +0000

    Mon, Oct 31, 2022, 12:40 AM +00:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -3,12 +3,23 @@
 #include <string.h>
 
 const int BITS_IN_BYTE = 8;
+int binary[8];
 
 void print_bulb(int bit);
 
 int main(void)
 {
     // TODO
+    //prompt user inputs (string)
+    string message = get_string("Message: ");
+
+    //extract each character and convert to binary
+    int i = 0;
+    while (message[i] != '\0')
+    {
+        binary[]
+    }
+
 }
 
 void print_bulb(int bit)
