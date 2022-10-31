f8e1b51a3c3a2834aa87e78156b44264577f9f19
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-31 16:22:47 +0000

    Mon, Oct 31, 2022, 4:22 PM +00:00

--- problem-sets/wordle/wordle.c
+++ problem-sets/wordle/wordle.c
@@ -162,6 +162,7 @@ int check_word(string guess, int wordsize, int status[], string choice)
             if (guess[i] == choice[i])
             {
                 int curr_score = EXACT;
+                //status[j] = 0;
                 status[i] = curr_score;
                 score += curr_score;
                 break;
