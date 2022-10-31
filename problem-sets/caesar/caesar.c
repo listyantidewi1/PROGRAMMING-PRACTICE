298a9e223476e1ef444e0714d62fd23fcf0c34ff
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-10-31 09:09:44 +0000

    Mon, Oct 31, 2022, 9:09 AM +00:00

--- problem-sets/caesar/caesar.c
+++ problem-sets/caesar/caesar.c
@@ -7,20 +7,6 @@ int main(int argc, string argv[])
 {
     //formula: c = (p+k)%26
     //c = chipertext, k = key, p = plaintext
-    while(argc==2)
-    {
-        string _argv = argv[1];
-        int length = strlen(_argv);
-        for (int i = 0; i < length; i++)
-        {
-            if ((_argv[i] < 48) || (_argv[i] > 57))
-            {
-                break;
-            }
-        }
-
-    }
-
-    printf("Usage: ./caesar key\n");
+ 
 
 }
