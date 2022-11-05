8045a80b5bbce175912724546013139dc86e7e3d
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-11-05 17:30:36 +0000

    Sat, Nov 5, 2022, 5:30 PM +00:00

--- problem-sets/caesar/caesar.c
+++ problem-sets/caesar/caesar.c
@@ -4,6 +4,8 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+bool only_digits(string s);
+
 int main(int argc, string argv[])
 {
     //formula: c = (p+k) %26
@@ -11,14 +13,29 @@ int main(int argc, string argv[])
     string _argv = argv[1];
 
 
-    if ((argc != 2) || (isalpha(_argv[1])))
-    {
-        printf("Usage: ./caesar key\n");
-    }
-    else
+    if ((argc == 2) || (only_digits(_argv)))
     {
         printf("OK\n");
     }
+    else
+    {
+        printf("Usage: ./caesar key\n");
+    }
 
+}
 
+bool only_digits(string s)
+{
+    for (int i=0, length = strlen(s); i < length; i++)
+    {
+        if(isalpha(s[i])){
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+
+    }
+    return 0;
 }
