a01775d86ee6998b8ea75a3a3bed07ada705d16d
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-11-05 17:13:22 +0000

    Sat, Nov 5, 2022, 5:13 PM +00:00

--- problem-sets/caesar/caesar.c
+++ problem-sets/caesar/caesar.c
@@ -9,7 +9,10 @@ int main(int argc, string argv[])
     //formula: c = (p+k) %26
     //c = chipertext, k = key, p = plaintext
 
-    if((argc != 2) || argv[1]<)
+    if ((argc != 2) || ((atoi(argv[1]))<48) || ((atoi(argv[1]))>57))
+    {
+        printf("Usage: ./caesar key\n");
+    }
 
 
 }
