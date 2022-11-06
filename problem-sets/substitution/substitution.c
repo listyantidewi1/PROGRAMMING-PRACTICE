f751c81378db5a08621559c65f7a7b60974eb3d3
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-11-06 12:08:15 +0000

    Sun, Nov 6, 2022, 12:08 PM +00:00

--- problem-sets/substitution/substitution.c
+++ problem-sets/substitution/substitution.c
@@ -1,13 +1,15 @@
 #include <cs50.h>
+#include <string.h>
+#include <ctype.h>
 #include <stdio.h>
 
 int main(int argc, string argv[])
 {
-    string plaintext;
+    string plaintext, ciphertext = "";
     string _argv =  argv[1];
     if (argc == 2 && (strlen(_argv) == 26))
     {
-        for (int i, length = strlen(_argv); i < length; i++)
+        for (int i = 0, length = strlen(_argv); i < length; i++)
         {
             if((_argv[i] < 65 || _argv[i] > 90) || (_argv[i] < 97 || _argv[i] > 122))
             {
@@ -17,6 +19,19 @@ int main(int argc, string argv[])
         }
 
         plaintext = get_string("plaintext:  ");
+
+        for (int i=0, length = strlen(plaintext); i < length; i++)
+        {
+            ciphertext[i] = _argv[i];
+        }
+        printf("ciphertext: ");
+
+        for (int i=0, length = strlen(ciphertext); i < length; i++)
+        {
+            printf("%c", ciphertext[i]);
+        }
+
+        printf("\n");
 
     }
     else if (strlen(_argv) != 26)
