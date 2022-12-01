4d547188c810ad2fb249ed3f8a9a9db2641c33ed
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-12-01 06:54:02 +0000

    Thu, Dec 1, 2022, 6:54 AM +00:00

--- problem-sets/reverse/reverse.c
+++ problem-sets/reverse/reverse.c
@@ -20,6 +20,19 @@ int main(int argc, char *argv[])
 
     // Open input file for reading
     // TODO #2
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
 
     // Read header into an array
     // TODO #3
