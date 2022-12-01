63c11d8e0e65ba497c9fe3d327190a09417bcc4a
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-12-01 06:38:46 +0000

    Thu, Dec 1, 2022, 6:38 AM +00:00

--- labs/volume/volume.c
+++ labs/volume/volume.c
@@ -37,8 +37,8 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    fread(header, HEADER_SIZE, 1, input);
-    fwrite(header, HEADER_SIZE, 1, output);
+    fread(headers, HEADER_SIZE, 1, input);
+    fwrite(headers, HEADER_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
     while (fread(&samples, sizeof(int16_t), 1, input))
