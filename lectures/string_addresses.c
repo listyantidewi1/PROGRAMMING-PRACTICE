112d591d03bd6ffe063a7d9f78f35403f89309ad
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-12-05 21:05:02 +0000

    Mon, Dec 5, 2022, 9:05 PM +00:00

--- lectures/string_addresses.c
+++ lectures/string_addresses.c
@@ -3,5 +3,12 @@
 int main(void)
 {
     char *s = "HI!";
-    printf("%s\n", s);
+    printf("%s\n", s); //print the string value at the address of s
+
+    //print the addresses of each character in string
+    printf("%p\n", s);
+    printf("%p\n", &s[0]);
+    printf("%p\n", &s[1]);
+    printf("%p\n", &s[2]);
+    printf("%p\n", &s[3]);
 }
