0ee0e5251eb9c2090e36fe6e7f0716a0b2d67f37
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-12-05 20:46:31 +0000

    Mon, Dec 5, 2022, 8:46 PM +00:00

--- lectures/addresses.c
+++ lectures/addresses.c
@@ -3,6 +3,7 @@
 int main(void)
 {
     int n = 50; //deklarasi variabel n dengan nilai 50
-    int *p = &n;
-    printf("%i\n", *p);
+    int *p = &n; //deklarasi pointer p dengan nilai alamat dari variabel n
+    printf("%i\n", *p); //print nilai yang berapa di alamat p //go to address p and print its value in integer
+    printf("%p\n", p); //print the balue of p which is the address of n
 }
