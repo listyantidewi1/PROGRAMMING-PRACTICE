59999212bf340a23c0073ecaed52ccd62e9c57ab
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-12-06 05:03:44 +0000

    Tue, Dec 6, 2022, 5:03 AM +00:00

--- lectures/swap.c
+++ lectures/swap.c
@@ -8,13 +8,14 @@ int main(void)
     int y = 2;
 
     printf("x is %i, y is %i\n", x, y);
-    swap(&x, &y);
-    printf("%x is %i, y is %i\n", x, y);
+    swap(&x, &y); //send the address of x and y to swap function
+    printf("x is %i, y is %i\n", x, y);
 
 }
 
-void swap(int *a, int *b)
+void swap(int *a, int *b) //take the address of x and y, use it as pointers a and b (point towards the address of x and y)
 {
+    //swap the actual values!
     int tmp = *a;
     *a = *b;
     *b = tmp;
