548a419cc1bdfdba4d9cb0d46d7808f93d0480a2
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-12-06 10:55:42 +0000

    Tue, Dec 6, 2022, 10:55 AM +00:00

--- problem-sets/filter-less/helpers.c
+++ problem-sets/filter-less/helpers.c
@@ -4,12 +4,20 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0; i < height; i++)
+    //the average RGB values should stay the same for the old and new image
+    //to make greyscale image, RGB values should be equal
+    float avg_rgb_old;
+    long temp = 0;
+
+    for(int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width; j++)
-        {
-            int rgbt = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
-            image[i][j].rgbtBlue = image[i][j].rgbtGreen = image[i][j].rgbtRed = rgbt;
+        for(int j=0; j<width; j++){
+            avg_rgb_old = round((double)(image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen)/3);
+            //int avg = ceil(avg_rgb_old);
+            //printf("%f\n", avg_rgb_old);
+            image[i][j].rgbtRed = avg_rgb_old;
+            image[i][j].rgbtBlue = avg_rgb_old;
+            image[i][j].rgbtGreen = avg_rgb_old;
         }
     }
     return;
@@ -26,9 +34,29 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             int sepiaG = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
             int sepiaR = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
 
-            image[i][j].rgbtBlue = (sepiaB > 255) ? 255 : sepiaB;
-            image[i][j].rgbtGreen = (sepiaG > 255) ? 255 : sepiaG;
-            image[i][j].rgbtRed = (sepiaR > 255) ? 255 : sepiaR;
+
+            if(sepiaB > 255){
+                image[i][j].rgbtBlue = 255;
+            }else{
+                image[i][j].rgbtBlue = sepiaB;
+            }
+
+            if(sepiaG > 255){
+                image[i][j].rgbtGreen = 255;
+            }else{
+                image[i][j].rgbtGreen = sepiaG;
+            }
+
+            if(sepiaR > 255){
+                image[i][j].rgbtRed = 255;
+            }else{
+                image[i][j].rgbtRed = sepiaR;
+            }
+
+            // image[i][j].rgbtBlue = (sepiaB > 255) ? 255 : sepiaB;
+            // image[i][j].rgbtGreen = (sepiaG > 255) ? 255 : sepiaG;
+            // image[i][j].rgbtRed = (sepiaR > 255) ? 255 : sepiaR;
+
         }
     }
     return;
@@ -43,18 +71,13 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for(int j = 0; j < width / 2; j++)
         {
-            // RGBTRIPLE *a = &image[i][j];
-            // RGBTRIPLE *b = &image[i][width - j -1];
-            // RGBTRIPLE tmp;
+
             // //swap here
-            // tmp = *a;
-            // *a = *b;
-            // *b = tmp;
 
             RGBTRIPLE tmp[width][height];
 
             tmp[i][j] = image[i][j];
-            image[i][j] = image[i][width-(j+1)]; //this cause segmentation error
+            image[i][j] = image[i][width-(j+1)];
             image[i][width-(j+1)] = tmp[i][j];
 
         }
