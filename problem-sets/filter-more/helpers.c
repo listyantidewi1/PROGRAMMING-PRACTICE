d8859a46cec218adb2eb9d3cade27eccebd85090
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-12-06 01:25:15 +0000

    Tue, Dec 6, 2022, 1:25 AM +00:00

--- problem-sets/filter-more/helpers.c
+++ problem-sets/filter-more/helpers.c
@@ -13,7 +13,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     for(int i = 0; i < height; i++)
     {
         for(int j=0; j<width; j++){
-            avg_rgb_old = ceil((double)(image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen)/3);
+            avg_rgb_old = round((double)(image[i][j].rgbtRed + image[i][j].rgbtBlue + image[i][j].rgbtGreen)/3);
             //int avg = ceil(avg_rgb_old);
             //printf("%f\n", avg_rgb_old);
             image[i][j].rgbtRed = avg_rgb_old;
