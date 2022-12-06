274081b58a4c1a3e947799c2c9063eed56191b7a
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-12-06 11:15:14 +0000

    Tue, Dec 6, 2022, 11:15 AM +00:00

--- problem-sets/filter-less/helpers.c
+++ problem-sets/filter-less/helpers.c
@@ -7,7 +7,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     //the average RGB values should stay the same for the old and new image
     //to make greyscale image, RGB values should be equal
     float avg_rgb_old;
-    long temp = 0;
+    long tmp = 0;
 
     for(int i = 0; i < height; i++)
     {
@@ -88,16 +88,16 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    RGBTRIPLE temp[height][width];
+    RGBTRIPLE tmp[height][width];
 
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            float sumBlue = 0;
-            float sumGreen = 0;
-            float sumRed = 0;
-            float counter = 0;
+            float totalBlue = 0;
+            float totalGreen = 0;
+            float totalRed = 0;
+            float count = 0;
 
             for (int r = -1; r < 2; r++)
             {
@@ -113,28 +113,22 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                         continue;
                     }
 
-                    sumBlue += image[i + r][j + c].rgbtBlue;
-                    sumGreen += image[i + r][j + c].rgbtGreen;
-                    sumRed += image[i + r][j + c].rgbtRed;
-                    counter++;
+                    totalBlue += image[i + r][j + c].rgbtBlue;
+                    totalGreen += image[i + r][j + c].rgbtGreen;
+                    totalRed += image[i + r][j + c].rgbtRed;
+                    count++;
                 }
             }
 
-            temp[i][j].rgbtBlue = round(sumBlue / counter);
-            temp[i][j].rgbtGreen = round(sumGreen / counter);
-            temp[i][j].rgbtRed = round(sumRed / counter);
-        }
-    }
+            tmp[i][j].rgbtBlue = round(totalBlue / count);
+            image[i][j].rgbtBlue = tmp[i][j].rgbtBlue;
 
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            image[i][j].rgbtBlue = temp[i][j].rgbtBlue;
-            image[i][j].rgbtGreen = temp[i][j].rgbtGreen;
-            image[i][j].rgbtRed = temp[i][j].rgbtRed;
-        }
+            tmp[i][j].rgbtGreen = round(totalGreen / count);
+            image[i][j].rgbtGreen = tmp[i][j].rgbtGreen;
 
+            tmp[i][j].rgbtRed = round(totalRed / count);
+            image[i][j].rgbtRed = tmp[i][j].rgbtRed;
+        }
     }
 
     return;
