058da67664f497c1ed317121d2fa397190b48aac
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-12-06 00:29:28 +0000

    Tue, Dec 6, 2022, 12:29 AM +00:00

--- labs/smiley/helpers.c
+++ labs/smiley/helpers.c
@@ -10,7 +10,9 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
             //make black pixels turn red
             if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
             {
-                image[i][j].rgbtRed = 0xff;
+                image[i][j].rgbtRed = 18;
+                image[i][j].rgbtBlue = 87;
+                image[i][j].rgbtGreen = 192;
             }
         }
     }
