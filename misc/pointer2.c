4382d516f6e7ea5f8259f79d67b3893230cc13ff
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-12-06 12:54:03 +0000

    Tue, Dec 6, 2022, 12:54 PM +00:00

--- misc/pointer2.c
+++ misc/pointer2.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-void square(int *x);
+void square(int *x); //prototipe fungsi
 
 int main(void)
 {
-    int x = 5;
-    square(&x);
-    printf("%i\n", x);
+    int x = 5; //deklarasi variabel x = 5
+    square(&x); //kirim alamat x ke *n di fungsi square
+    printf("%i\n", x); //25
 }
 
-void square(int *n)
+void square(int *n) //alamat x diterima oleh *n
 {
-    *n = *n * *n;
+    *n = *n * *n; //apapun yg ada di alamat x atau di *n, dikuadratkan
 }
