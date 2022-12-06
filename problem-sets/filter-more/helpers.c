d88846ae85dc4ef30a10eb803b4d1eed2b2433e2
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-12-06 03:57:15 +0000

    Tue, Dec 6, 2022, 3:57 AM +00:00

--- problem-sets/filter-more/helpers.c
+++ problem-sets/filter-more/helpers.c
@@ -34,7 +34,10 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 
     for (int i = 0; i < height; i++)
     {
-        for(int j=0; j<)
+        for(int j=0; j< width; j++)
+        {
+            
+        }
     }
 
 
