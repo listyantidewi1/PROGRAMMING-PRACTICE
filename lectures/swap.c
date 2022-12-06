bb7d9d365cbfadbb83d279c9bc50b6ff82fb88da
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-12-06 13:27:00 +0000

    Tue, Dec 6, 2022, 1:27 PM +00:00

--- lectures/swap.c
+++ lectures/swap.c
@@ -5,12 +5,13 @@
       int x = 1; //index kiri atas
       int y = 2; //index kanan atas
 
+      printf("x is %i, y is %i\n", x, y);
+
+      //swap
       int tmp = x;
       x = y;
       y = tmp;
 
-      printf("x is %i, y is %i\n", x, y);
-      //swap(&x, &y);
       printf("x is %i, y is %i\n", x, y);
   }
 
