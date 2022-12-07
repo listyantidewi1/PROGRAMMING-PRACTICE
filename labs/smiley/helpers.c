e60b0c1cbc6bf64b4a1f010254f0ed7e602e2ca8
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-12-07 12:00:42 +0000

    Wed, Dec 7, 2022, 12:00 PM +00:00

--- labs/smiley/helpers.c
+++ labs/smiley/helpers.c
@@ -10,8 +10,9 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
             if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
             {
                 image[i][j].rgbtRed = 18;
-                image[i][j].rgbtBlue = 87;
-                image[i][j].rgbtGreen = 192;
+                image[i][j].rgbtGreen = 18;
+                image[i][j].rgbtBlue = 18;
+
             }
         }
     }
