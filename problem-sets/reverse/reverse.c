2e5f8cbb34f093be052e78116f55177bab1d4ef7
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-12-11 18:06:42 +0000

    Sun, Dec 11, 2022, 6:06 PM +00:00

--- problem-sets/reverse/reverse.c
+++ problem-sets/reverse/reverse.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 {
     // Ensure proper usage
     // TODO #1
-    if (argc != 2)
+    if (argc != 3)
     {
         printf("Usage: ./reverse input.wav output.wav\n");
         return 1;
