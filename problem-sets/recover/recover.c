e0d42744f20472c89880ed9194fdc7ba729d7379
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-12-11 18:47:51 +0000

    Sun, Dec 11, 2022, 6:47 PM +00:00

--- problem-sets/recover/recover.c
+++ problem-sets/recover/recover.c
@@ -28,7 +28,10 @@ int main(int argc, char *argv[])
 
     while (fread(buffer, 512, 1, input_pointer))
     {
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        /*
+        Even though JPEGs are more complicated than BMPs, JPEGs have “signatures,” patterns of bytes that can distinguish them from other file formats. Specifically, the first three bytes of JPEGs are 0xff 0xd8 0xff from first byte to third byte, left to right. The fourth byte, meanwhile, is either 0xe0, 0xe1, 0xe2, 0xe3, 0xe4, 0xe5, 0xe6, 0xe7, 0xe8, 0xe9, 0xea, 0xeb, 0xec, 0xed, 0xee, or 0xef. Put another way, the fourth byte’s first four bits are 1110.
+        */
+        if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] & 0xf0) == 0xe0))
         {
             if (output_pointer != 0)
             {
