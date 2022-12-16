26b479edb32db1739a9e7e9a3be76abb8c386e0d
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2022-12-16 10:05:45 +0000

    Fri, Dec 16, 2022, 10:05 AM +00:00

--- labs/inheritance/inheritance.c
+++ labs/inheritance/inheritance.c
@@ -1,112 +1,94 @@
-// Simulate genetic inheritance of blood type
 
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-//ini buat allele 2 buat setiap orang
-typedef struct manusia
+typedef struct person
 {
-    struct manusia *parents[2];
+    struct person *parents[2];
     char alleles[2];
 }
-manusia;
+person;
 
 const int GENERATIONS = 3;
 const int INDENT_LENGTH = 4;
 
-manusia *create_family(int generations);
-void print_family(manusia *p, int generation);
-void free_family(manusia *p);
+person *create_family(int generations);
+void print_family(person *p, int generation);
+void free_family(person *p);
 char random_allele();
 
 int main(void)
 {
-    // kasih bilangan acak
     srand(time(0));
 
-    // buat sebuah family untuk tiga generasi
-    manusia *p = create_family(GENERATIONS);
+    person *p = create_family(GENERATIONS);
 
-    // cetak keluarga
     print_family(p, 0);
 
-    // Free memory
     free_family(p);
 }
 
-// fungsi utk membuat keluarga
-manusia *create_family(int generations)
+person *create_family(int generations)
 {
-    //alokasi memory untuk manusia baru
-    manusia *manusia_baru = malloc(sizeof(manusia));
+    person *new_person = malloc(sizeof(person));
 
-    if (manusia_baru == NULL)
+    if (new_person == NULL)
     {
-        printf("Memory error while creating new manusia.\n");
+        printf("Memory error while creating new person.\n");
         return NULL;
     }
 
-
-    if (generations >= 2)
+    if (generations > 1)
     {
+        person *parent0 = create_family(generations - 1);
+        person *parent1 = create_family(generations - 1);
 
-        manusia *parent0 = create_family(generations - 1);
-        manusia *parent1 = create_family(generations - 1);
+        new_person->parents[0] = parent0;
+        new_person->parents[1] = parent1;
 
-        manusia_baru->parents[0] = parent0;
-        manusia_baru->parents[1] = parent1;
-
-        manusia_baru->alleles[0] = parent0->alleles[rand() % 2];
-        manusia_baru->alleles[1] = parent1->alleles[rand() % 2];
+        new_person->alleles[0] = parent0->alleles[rand() % 2];
+        new_person->alleles[1] = parent1->alleles[rand() % 2];
     }
 
     else
     {
-        manusia_baru->parents[0] = NULL;
-        manusia_baru->parents[1] = NULL;
+        new_person->parents[0] = NULL;
+        new_person->parents[1] = NULL;
 
-        manusia_baru->alleles[0] = random_allele();
-        manusia_baru->alleles[1] = random_allele();
+        new_person->alleles[0] = random_allele();
+        new_person->alleles[1] = random_allele();
     }
 
-    return manusia_baru;
+    return new_person;
 }
 
-// Free `p` and all ancestors of `p`.
-void free_family(manusia *p)
+void free_family(person *p)
 {
-    // Handle base case
     if (p == NULL)
     {
         return;
     }
 
-    // Free parents recursively
     free_family(p->parents[0]);
     free_family(p->parents[1]);
 
-    // Free child
     free(p);
 }
 
-// Print each family member and their alleles.
-void print_family(manusia *p, int generation)
+void print_family(person *p, int generation)
 {
-    // Handle base case
     if (p == NULL)
     {
         return;
     }
 
-    // Print indentation
     for (int i = 0; i < generation * INDENT_LENGTH; i++)
     {
         printf(" ");
     }
 
-    // Print manusia
     if (generation == 0)
     {
         printf("Child (Generation %i): blood type %c%c\n", generation, p->alleles[0], p->alleles[1]);
@@ -124,12 +106,10 @@ void print_family(manusia *p, int generation)
         printf("Grandparent (Generation %i): blood type %c%c\n", generation, p->alleles[0], p->alleles[1]);
     }
 
-    // Print parents of current generation
     print_family(p->parents[0], generation + 1);
     print_family(p->parents[1], generation + 1);
 }
 
-// Randomly chooses a blood type allele.
 char random_allele()
 {
     int r = rand() % 3;
