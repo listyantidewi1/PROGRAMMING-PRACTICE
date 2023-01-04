bdb1e9deffba05a4eb111ffe4cbffc5cf0da7d07
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-01-04 13:05:07 +0000

    Wed, Jan 4, 2023, 1:05 PM +00:00

--- sections/section-6/conditional.py
+++ sections/section-6/conditional.py
@@ -1,6 +1,6 @@
 import sys
 
-if int(sys.argv[1] > 10):
+if int(sys.argv[1]) > 10:
     print("You enter a number which is more than 10")
-elif int(sys.argv[1] < 5):
+elif int(sys.argv[1]) < 5:
     print("You enter a number which is less than 5")
