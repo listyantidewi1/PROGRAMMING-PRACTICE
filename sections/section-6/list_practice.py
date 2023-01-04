32133d17dffd53007fb71dbf66cb346864db7582
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-01-04 13:23:04 +0000

    Wed, Jan 4, 2023, 1:23 PM +00:00

---
+++ sections/section-6/list_practice.py
@@ -0,0 +1,41 @@
+from random import randint
+import sys
+MAX_VALUE = 20
+LIST_LENGTH = 50
+
+
+def main():
+    # Create a list of LIST_LENGTH random numbers, 0 to MAX_VALUE
+    random_nums = [randint(0,MAX_VALUE) for x in range(LIST_LENGTH)]
+
+    # Print the list
+
+    # TODO: Count occurences of '0' in the list
+
+    # TODO: Print the 15th element of the list
+
+    # TODO: Print the length of the list
+
+
+    # Slicing!
+    # TODO: Print the first 10 items in the list:
+
+    # TODO: Print the last 10 items in the list
+
+    # TODO: Print every third item in the list:
+
+
+    # Exercise:
+
+    # TODO 1: Print the values in Reverse-order
+
+
+    # TODO 2: Print the even values of the list
+
+
+    # TODO 3: Print the odd-indexed values of the list
+
+
+
+if __name__ == "__main__":
+    main()
