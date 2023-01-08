6f7a4ca0b0818df29a13a1cad466707d97308b35
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-01-08 15:28:19 +0000

    Sun, Jan 8, 2023, 3:28 PM +00:00

--- problem-sets/sentimental-credit/credit.py
+++ problem-sets/sentimental-credit/credit.py
@@ -1,1 +1,14 @@
 # TODO
+
+# ask for credit card number
+
+number = input("Number: ")
+
+# count number of digits
+length = 0
+
+for i in range(0, len(number)):
+    if number[i].isdigit() = True:
+        length += 1
+
+print(length)
