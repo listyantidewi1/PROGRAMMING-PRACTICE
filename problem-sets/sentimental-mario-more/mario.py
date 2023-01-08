ed461fbad86912803d5aace8845a49faba6be722
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-01-08 14:29:17 +0000

    Sun, Jan 8, 2023, 2:29 PM +00:00

--- problem-sets/sentimental-mario-more/mario.py
+++ problem-sets/sentimental-mario-more/mario.py
@@ -8,7 +8,7 @@ height = 0
 height = input("Height: ")
 
 # keep asking for height if its out of required values
-if int(height) < 1 or int(height) > 8:
+if (int(height) < 1 or int(height) > 8) and height.isdigit():
     height = input("Height: ")
 
 # loop for the rows
