183751f9b524f96ad5eebd62ba11b951a66856f3
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-01-08 14:22:07 +0000

    Sun, Jan 8, 2023, 2:22 PM +00:00

--- problem-sets/sentimental-mario-more/mario.py
+++ problem-sets/sentimental-mario-more/mario.py
@@ -1,18 +1,26 @@
 # TODO
+# recreate mario more
 
+# initial height
 height = 0
 
+# ask for height as input
 height = input("Height: ")
 
+# keep asking for height if its out of required values
 if int(height) < 0 or int(height) > 8:
     height = input("Height: ")
 
+# loop for the rows
 for i in range(0, int(height)):
+    # loop for the space
     for j in range(int(height), (i + 1), -1):
         print(" ", end="")
-    for k in range(0,(i+1)):
+
+    # loop for the #
+    for k in range(0, (i+1)):
         print("#", end="")
-    print(" ")
+    print(" ", end="")
     for l in range(0, (i+1)):
         print("#", end="")
-    print("\n")
+    print("\n", end="")
