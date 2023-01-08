dba444dc724bca2a3f4966f3f4c83045bf6a802a
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-01-08 14:54:11 +0000

    Sun, Jan 8, 2023, 2:54 PM +00:00

--- problem-sets/sentimental-readability/readability.py
+++ problem-sets/sentimental-readability/readability.py
@@ -15,5 +15,18 @@ num_words = len(text.split())
 # print(num_words)
 
 # count the number of sentences
-num_sentences = len(text.split('.'))
-print(num_sentences)
+num_sentences = text.count('.')
+# print(num_sentences)
+
+# compute L
+
+l = num_letters / num_words * 100
+s = num_sentences / num_words * 100
+index = 0.0588 * l - 0.296 * s - 15.8
+
+if index < 1:
+    print("Before Grade 1")
+elif index > 16:
+    print("Grade 16+")
+else:
+    print("Grade ", index)
