54d2c50b2c5ff519c3d4aeffd02399d7578fc8e8
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-01-08 14:25:10 +0000

    Sun, Jan 8, 2023, 2:25 PM +00:00

--- problem-sets/sentimental-mario-more/mario.py
+++ problem-sets/sentimental-mario-more/mario.py
@@ -8,7 +8,7 @@ height = 0
 height = input("Height: ")
 
 # keep asking for height if its out of required values
-if int(height) < 0 or int(height) > 8:
+if int(height) < 1 or int(height) > 8:
     height = input("Height: ")
 
 # loop for the rows
@@ -20,7 +20,7 @@ for i in range(0, int(height)):
     # loop for the #
     for k in range(0, (i+1)):
         print("#", end="")
-    print(" ", end="")
+    print("  ", end="")
     for l in range(0, (i+1)):
         print("#", end="")
     print("\n", end="")
