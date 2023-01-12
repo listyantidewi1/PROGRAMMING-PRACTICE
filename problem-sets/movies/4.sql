559ea589eb71c2becb1cd760de919c86aa7c70cd
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-01-12 07:27:23 +0000

    Thu, Jan 12, 2023, 7:27 AM +00:00

--- problem-sets/movies/4.sql
+++ problem-sets/movies/4.sql
@@ -1,2 +1,3 @@
 --In 4.sql, write a SQL query to determine the number of movies with an IMDb rating of 10.0.
 --our query should output a table with a single column and a single row (not counting the header) containing the number of movies with a 10.0 rating.
+select count(movie_id) from ratings where rating = 10.0;
