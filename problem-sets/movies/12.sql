11ae9452c0c1e99c740d35e0dbfae50c95854751
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-01-12 07:53:15 +0000

    Thu, Jan 12, 2023, 7:53 AM +00:00

--- problem-sets/movies/12.sql
+++ problem-sets/movies/12.sql
@@ -2,3 +2,5 @@
 --Your query should output a table with a single column for the title of each movie.
 --You may assume that there is only one person in the database with the name Johnny Depp.
 --You may assume that there is only one person in the database with the name Helena Bonham Carter.
+
+select title from movies inner join stars on movies.id = stars.movie_id inner join people on stars.person_id = people.id where name = 'Johnny Depp' or name = 'Helena Bonham Carter'
