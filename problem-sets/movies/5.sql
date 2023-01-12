ab4adea2433d1b15adf77eebd06df7b8e569b96b
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-01-12 07:29:59 +0000

    Thu, Jan 12, 2023, 7:29 AM +00:00

--- problem-sets/movies/5.sql
+++ problem-sets/movies/5.sql
@@ -2,3 +2,4 @@
 --Your query should output a table with two columns, one for the title of each movie and one for the release year of each movie.
 --You may assume that the title of all Harry Potter movies will begin with the words “Harry Potter”, and that if a movie title begins with the words “Harry Potter”, it is a Harry Potter movie.
 
+select title, year from movies where title like 'Harry Potter%' order by year asc;
