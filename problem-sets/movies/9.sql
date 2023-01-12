a3fadfc92d40faa0abd7af8d49d0f34e2bb22178
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-01-12 07:44:06 +0000

    Thu, Jan 12, 2023, 7:44 AM +00:00

--- problem-sets/movies/9.sql
+++ problem-sets/movies/9.sql
@@ -3,3 +3,5 @@
 --People with the same birth year may be listed in any order.
 --No need to worry about people who have no birth year listed, so long as those who do have a birth year are listed in order.
 --If a person appeared in more than one movie in 2004, they should only appear in your results once.
+
+select distinct name from people inner join stars on people.id = stars.person_id inner join movies on stars.movie_id = movies.id where year = 2004 order by birth asc;
