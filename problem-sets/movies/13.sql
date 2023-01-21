fd7eb17b5d010d495ff616770ad1c3b76a9cdea9
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-01-21 16:38:21 +0000

    Sat, Jan 21, 2023, 4:38 PM +00:00

--- problem-sets/movies/13.sql
+++ problem-sets/movies/13.sql
@@ -2,3 +2,13 @@
 --Your query should output a table with a single column for the name of each person.
 --There may be multiple people named Kevin Bacon in the database. Be sure to only select the Kevin Bacon born in 1958.
 --Kevin Bacon himself should not be included in the resulting list.l
+select name from people inner join stars
+on people.id = stars.person_id
+inner join movies
+on stars.movie_id = movies.id
+where stars.movie_id =
+(select movies.id from movies inner join stars
+on movies.id = stars.movie_id
+inner join people
+on stars.person_id = people.id
+where name = "Kevin Bacon" and birth = 1958) and name != "Kevin Bacon";
