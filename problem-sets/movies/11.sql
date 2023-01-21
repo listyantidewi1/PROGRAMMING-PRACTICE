3e90b34b5144c5dd919f7574a00703348c411a91
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-01-21 16:18:42 +0000

    Sat, Jan 21, 2023, 4:18 PM +00:00

--- problem-sets/movies/11.sql
+++ problem-sets/movies/11.sql
@@ -2,4 +2,4 @@
 --Your query should output a table with a single column for the title of each movie.
 --You may assume that there is only one person in the database with the name Chadwick Boseman.
 
-select title from movies inner join stars on movies.id = stars.movie_id inner join ratings on stars.movie_id = ratings.movie_id inner join people on stars.person_id = people.id where name = 'Chadwick Boseman' order by rating asc limit 5;
+select title from movies inner join stars on movies.id = stars.movie_id inner join ratings on stars.movie_id = ratings.movie_id inner join people on stars.person_id = people.id where name = 'Chadwick Boseman' order by rating desc limit 5;
