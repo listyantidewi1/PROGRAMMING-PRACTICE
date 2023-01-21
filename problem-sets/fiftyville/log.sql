b3395b8ca77f7f8f626af6872ca05cad24a758f3
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-01-21 17:15:06 +0000

    Sat, Jan 21, 2023, 5:15 PM +00:00

--- problem-sets/fiftyville/log.sql
+++ problem-sets/fiftyville/log.sql
@@ -20,3 +20,21 @@ select * from interviews where transcript like '%bakery%';
 --| 161 | Ruth    | 2021 | 7     | 28  | Sometime within ten minutes of the theft, I saw the thief get into a car in the bakery parking lot and drive away. If you have security footage from the bakery parking lot, you might want to look for cars that left the parking lot in that time frame.                                                          |
 --| 162 | Eugene  | 2021 | 7     | 28  | I don't know the thief's name, but it was someone I recognized. Earlier this morning, before I arrived at Emma's bakery, I was walking by the ATM on Leggett Street and saw the thief there withdrawing some money.                                                                                                 |
 --| 163 | Raymond | 2021 | 7     | 28  | As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket. |
+
+select * from bakery_security_logs where year = 2021 and month = 7 and day = 28 and hour = 10;
+
+/*+-----+------+-------+-----+------+--------+----------+---------------+
+| id  | year | month | day | hour | minute | activity | license_plate |
++-----+------+-------+-----+------+--------+----------+---------------+
+| 258 | 2021 | 7     | 28  | 10   | 8      | entrance | R3G7486       |
+| 259 | 2021 | 7     | 28  | 10   | 14     | entrance | 13FNH73       |
+| 260 | 2021 | 7     | 28  | 10   | 16     | exit     | 5P2BI95       |
+| 261 | 2021 | 7     | 28  | 10   | 18     | exit     | 94KL13X       |
+| 262 | 2021 | 7     | 28  | 10   | 18     | exit     | 6P58WS2       |
+| 263 | 2021 | 7     | 28  | 10   | 19     | exit     | 4328GD8       |
+| 264 | 2021 | 7     | 28  | 10   | 20     | exit     | G412CB7       |
+| 265 | 2021 | 7     | 28  | 10   | 21     | exit     | L93JTIZ       |
+| 266 | 2021 | 7     | 28  | 10   | 23     | exit     | 322W7JE       |
+| 267 | 2021 | 7     | 28  | 10   | 23     | exit     | 0NTHK55       |*/
+
+
