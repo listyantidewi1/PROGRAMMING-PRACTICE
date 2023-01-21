43e2ee86f09b967fbc1a8fd0dfebaf76143dc0d9
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-01-21 17:03:46 +0000

    Sat, Jan 21, 2023, 5:03 PM +00:00

--- problem-sets/fiftyville/log.sql
+++ problem-sets/fiftyville/log.sql
@@ -8,3 +8,7 @@
 --All you know is that the theft took place on July 28, 2021 and that it took place on Humphrey Street.
 
 --How will you go about solving this mystery? The Fiftyville authorities have taken some of the town’s records from around the time of the theft and prepared a SQLite database for you, fiftyville.db, which contains tables of data from around the town. You can query that table using SQL SELECT queries to access the data of interest to you. Using just the information in the database, your task is to solve the mystery.
+
+select * from crime_scene_reports where day = 28 and month = 7 and year = 2021 and street = "Humphrey Street";
+
+--Humphrey Street | Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery. |
