8c4e48093708cdf72373f3137a6a8a397d1c8147
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-02-04 12:09:02 +0000

    Sat, Feb 4, 2023, 7:09 PM +07:00

--- labs/birthdays/app.py
+++ labs/birthdays/app.py
@@ -9,7 +9,7 @@ db = SQL("sqlite:///birthdays.db")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
+    
 
         # Access form data
         name = request.form.get("name")
