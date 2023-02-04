842ef88d478289f583f4be97ea6d79ff9e3fd2e0
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-02-04 15:44:32 +0000

    Sat, Feb 4, 2023, 10:44 PM +07:00

--- labs/birthdays/app.py
+++ labs/birthdays/app.py
@@ -9,7 +9,7 @@ db = SQL("sqlite:///birthdays.db")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    
+    if request.method == "POST":
 
         # Access form data
         name = request.form.get("name")
@@ -27,5 +27,6 @@ def index():
         # Query for all birthdays
         birthdays = db.execute("SELECT * FROM birthdays")
 
+        print(birthdays)
         # Render birthdays page
         return render_template("index.html", birthdays=birthdays)
