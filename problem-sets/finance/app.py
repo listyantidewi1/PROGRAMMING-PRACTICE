b12e4851d7fedf4e53b5137007f38f8b3bbf45b0
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-02-04 21:48:08 +0000

    Sun, Feb 5, 2023, 4:48 AM +07:00

--- problem-sets/finance/app.py
+++ problem-sets/finance/app.py
@@ -188,6 +188,7 @@ def sell():
 
     if request.method == "GET":
         symbols = db.execute("Select symbol from trx where user_id = ?", id)
+        return render_template("sell.html", symbol = symbols)
 
     if request.method == "POST":
         if not request.form.get("symbol"):
