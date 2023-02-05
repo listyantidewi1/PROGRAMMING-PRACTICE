33d72c8950ffcb19a86c54b2fc7687b939fc579a
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-02-05 16:18:28 +0000

    Sun, Feb 5, 2023, 11:18 PM +07:00

--- problem-sets/finance/app.py
+++ problem-sets/finance/app.py
@@ -72,7 +72,7 @@ def buy():
             #bill = float(bill)
             db.execute("UPDATE users set cash = ? WHERE id = ?", new_cash, id)
             db.execute("insert into trx (user_id, symbol, shares, price) values (?, ?, ?, ?)", id, current_symbol["symbol"], shares, bill)
-            flash('Shares was sucessfully bought')
+            flash('Following stock was sucessfully bought')
             return render_template("index.html", symbols = current_symbol, shares = shares, bill = bill, new_cash = new_cash)
         else:
             return apology("not enough cash", 403)
