010266e61d1ffd9eb9316e8d14e59c9f4cad6994
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-02-05 19:47:42 +0000

    Mon, Feb 6, 2023, 2:47 AM +07:00

--- problem-sets/finance/app.py
+++ problem-sets/finance/app.py
@@ -242,9 +242,12 @@ def sell():
         db.execute("insert into trx (user_id, symbol, name, shares, price) values(?, ?, ?, ?, ?)", id, symbol_to_sell["symbol"], symbol_to_sell["name"], shares, abs(price_per_symbol * int(shares)))
 
         # update stock shares
-        current_stock = db.execute("select shares from purchased_stock where user_id = ? and symbol = ?", id, symbol_to_sell["symbol"])
+        current_stock = db.execute("select * from purchased_stock where user_id = ? and symbol = ?", id, symbol_to_sell["symbol"])
         print("current stock:", current_stock)
-        
+        if len(current_stock) > 0:
+            old_shares = db.execute("select shares from purchased_stock where user_id = ? and symbol = ?)
+
+
 
         flash("Sucessfully sold")
     return redirect("/")
