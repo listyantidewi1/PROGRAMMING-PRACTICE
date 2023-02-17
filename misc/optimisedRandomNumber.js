d3810369f040488d30c612831f0f3d44ef3a686b
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-02-17 07:34:08 +0000

    Fri, Feb 17, 2023, 2:34 PM +07:00

--- misc/optimisedRandomNumber.js
+++ misc/optimisedRandomNumber.js
@@ -1,2 +1,9 @@
 let number = Math.random();
 console.log(number)
+if(number <= 0.1){
+    console.log(1)
+}else if(number <= 0.2){
+    console.log(2)
+}else if(number <= 0.3){
+    console.log(3)
+}
