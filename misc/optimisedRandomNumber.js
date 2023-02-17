5536fe34cbe89771090cbf5777ca3bcec14d6df1
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-02-17 07:39:02 +0000

    Fri, Feb 17, 2023, 2:39 PM +07:00

--- misc/optimisedRandomNumber.js
+++ misc/optimisedRandomNumber.js
@@ -10,4 +10,14 @@ if(number <= 0.1){
     console.log(4)
 }else if(number <= 0.5){
     console.log(5)
+}else if(number <= 0.6){
+    console.log(6)
+}else if(number <= 0.7){
+    console.log(7)
+}else if(number <= 0.8){
+    console.log(8)
+}else if(number <= 0.9){
+    console.log(9)
+}else if(number <= 1){
+    console.log(10)
 }
