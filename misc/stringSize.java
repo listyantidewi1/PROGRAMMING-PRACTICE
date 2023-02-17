e33434ae5922b94e8d26c65e91df77f5d387914d
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-02-17 08:00:11 +0000

    Fri, Feb 17, 2023, 3:00 PM +07:00

---
+++ misc/stringSize.java
@@ -0,0 +1,10 @@
+class Str{
+    public static void main(String[] args){
+        int size =0;
+        String s = "hello";
+        for(int i = 0; i < s.length(); i++){
+            size++;
+        }
+        System.out.print(size);
+    }
+}
