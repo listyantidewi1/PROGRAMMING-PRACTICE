c89c1d8acf2ff76499b471e338981397f5a8b173
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-02-18 23:00:53 +0000

    Sun, Feb 19, 2023, 6:00 AM +07:00

--- test/js/mario.js
+++ test/js/mario.js
@@ -1,6 +1,14 @@
 var readlineSync = require('readline-sync');
 
 //wait for user's response
-var userName = readlineSync.question('May I have your name?');
-console.log('Hi ' + userName + '!');
+// var userName = readlineSync.question('May I have your name?');
+// console.log('Hi ' + userName + '!');
 
+var height;
+while((height <= 0) || (height > 8))
+{
+    height = readlineSync.question("Height: ");
+}
+
+//loop for the number of rows
+var i = 0;
