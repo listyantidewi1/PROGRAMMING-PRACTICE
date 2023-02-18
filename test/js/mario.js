eb2db42481a41246cf8aafe91db132f5e1c3d7fd
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-02-18 22:48:19 +0000

    Sun, Feb 19, 2023, 5:48 AM +07:00

--- test/js/mario.js
+++ test/js/mario.js
@@ -2,5 +2,5 @@ var readlineSync = require('readline-sync');
 
 //wait for user's response
 var userName = readlineSync.question('May I have your name?');
-console.log('Hi' + userName + '!');
+console.log('Hi ' + userName + '!');
 
