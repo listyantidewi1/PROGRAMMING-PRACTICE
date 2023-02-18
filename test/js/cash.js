98c0b2342c6c5200f5b017834fb7f9ece0f54421
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-02-18 23:29:49 +0000

    Sun, Feb 19, 2023, 6:29 AM +07:00

--- test/js/cash.js
+++ test/js/cash.js
@@ -13,4 +13,34 @@ calculate_quarters = (cents) => {
     return quarters;
 }
 
+calculate_dimes = (cents) => {
+    var dimes = cents / 10;
+    return dimes;
+}
+
+calculate_nickels = (cents) => {
+    var nickels = cents / 5;
+    return nickels;
+}
+
+calculate_pennies = (cents) => {
+    return cents;
+}
+
 var cents = get_cents();
+
+var quarters = calculate_quarters(cents);
+cents = cents - quarters * 25;
+
+var dimes = calculate_dimes(cents);
+cents = cents - dimes * 10;
+
+var nickels = calculate_nickels(cents);
+cents = cents - nickels * 5;
+
+var pennies = calculate_pennies(cents);
+cents = cents - pennies * 1;
+
+var coins = quarters + dimes + nickels + pennies;
+
+console.log(coins);
