35d3cf1c392697127a2176ff6ca47560b081b419
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-02-19 02:48:07 +0000

    Sun, Feb 19, 2023, 9:48 AM +07:00

--- test/nearby/find_availability.py
+++ test/nearby/find_availability.py
@@ -6,5 +6,5 @@ db = SQL("sqlite://amazon.db")
 def find_availability(item_id):
     """Returns a list of warehouses in which an item with `item_id` is in stock"""
     warehouse = db.execute(
-        "select warehouses.name from warehouses inner join items on warehouses.id = items.warehouse_id where stock >= 0 and items.id = ?", item_id)
+        "select warehouses.id from warehouses inner join items on warehouses.id = items.warehouse_id where stock >= 0 and items.id = ?", item_id)
     return warehouse
