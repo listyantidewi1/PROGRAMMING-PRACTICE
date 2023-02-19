94fd318f9beb4600054ee605566ad69a1f57537f
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-02-19 02:34:17 +0000

    Sun, Feb 19, 2023, 9:34 AM +07:00

--- test/nearby/helpers.py
+++ test/nearby/helpers.py
@@ -3,5 +3,5 @@ from cs50 import SQL
 db = SQL("sqlite://amazon.db")
 
 def find_availability(item_id):
-    """Returns a list of warehouses in which an item with `item_id` is in stocl"""
-    
+    """Returns a list of warehouses in which an item with `item_id` is in stock"""
+    db.execute("select warehouses.id, warehouses.name from warehouses inner join items on warehouse.id = items.warehouse_id where stock >= 0")
