8120d584cba5aa258830d606d7511f2b3e9a3e7c
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-02-19 00:56:45 +0000

    Sun, Feb 19, 2023, 7:56 AM +07:00

--- test/jack/push2.c
+++ test/jack/push2.c
@@ -1,4 +1,41 @@
+// #include <stdbool.h>
+
+
+// const int CAPACITY = 50;
+
+// typedef struct
+// {
+//     int numbers[CAPACITY];
+//     int size;
+// } stack;
+
+// stack s;
+// s.size = 0;
+
+// but why is s undefined?
+
 bool push(int n)
 {
     // TODO
+    // check if stack is already full, return false if true
+    if (s->size == CAPACITY)
+    {
+        return false;
+    }
+
+    if (s->size == sizeof(s->numbers) /  sizeof(int))
+    {
+        int new_capacity = sizeof(s->numbers) / sizeof(int) * 2;
+        int* new_numbers = realloc(s->numbers, new_capacity * sizeof(int));
+        if (new_numbers == NULL)
+        {
+            return false;
+        }
+        s->numbers = new_numbers;
+    }
+
+    //else, push n
+    s->numbers[s->size] = n;
+    s->size++;
+    return true;
 }
