be2fdeed8499cfc959fa92a5b79d2ff98086742a
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-02-19 02:24:21 +0000

    Sun, Feb 19, 2023, 9:24 AM +07:00

--- test/schema.sql
+++ test/schema.sql
@@ -1,8 +1,16 @@
 CREATE TABLE warehouses (
-    id int NOT NULL AUTO_INCREMENT,
+    id int(5) NOT NULL AUTO_INCREMENT,
     name varchar(255) NOT NULL,
     latitude varchar(255) NOT NULL,
     longitude varchar(255) NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
 
+CREATE TABLE items (
+    id int NOT NULL,
+    name varchar(255) NOT NULL,
+    stock int(5) NOT NULL,
+    warehouse_id int(5) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (warehouse_id) REFERENCES warehouse (id)
+)
