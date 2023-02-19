7582dce51701c53f3f3dea5eae6722eb061b3e43
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-02-19 00:37:34 +0000

    Sun, Feb 19, 2023, 7:37 AM +07:00

--- test/jack/push.c
+++ test/jack/push.c
@@ -1,4 +1,14 @@
 bool push(int n)
 {
     // TODO
+    // check if stack is already full, return false if true
+    if (s->size == CAPACITY)
+    {
+        return false;
+    }
+
+    //else, push n 
+    s->numbers[s->size] = n;
+    s->size++;
+    return true;
 }
