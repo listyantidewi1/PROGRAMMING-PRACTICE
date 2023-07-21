9e24cf44f879e5ceec426f8cb61afe26eefd3cfd
Listyanti Dewi Astuti <89518378+listyantidewi1@users.noreply.github.com>
2023-07-21 01:21:54 +0000

    Fri, Jul 21, 2023, 8:21 AM +07:00

--- dpk/mario.c
+++ dpk/mario.c
@@ -1,17 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int get_size(void);
+void print_grid(int n);
+
 int main(void)
 {
-    // tanyakan ukuran grid ke pengguna, dan simpan di variabel n
+    int n = get_size();
+    print_grid(n);
+}
+
+int get_size(void)
+{
     int n;
     do
     {
         n = get_int("Size: ");
     }
     while (n < 1);
+    return n;
+}
 
-    // Print balok
+void print_grid(int n)
+{
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
